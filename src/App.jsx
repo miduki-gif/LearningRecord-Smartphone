90bc25592f8222e01800c58df25b85e94309e0d8
miduki-gif <midzuki1935@gmail.com>
2024-08-31 14:50:24 +0900

    build-test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { InputForm } from "./components/InputForm";
-// import { ListDisplay } from "./components/ListDisplay";
 import { getLearnRecords } from "../utils/SupabaseFunction";
 import { RecordData } from "../utils/RecordData";
 import { supabase } from "../utils/Supabase";
@@ -12,12 +11,19 @@ export const App = () => {
   const [totalTime, setTotalTime] = useState(0);
   const [loading, setLoading] = useState(true);
 
+          //合計時間計算
+          const totalTimeCalculate = (records) => {
+            return records.reduce((sum, record) => sum + (record.time || 0), 0)
+          }
+
   useEffect(() => {
     const data = async () => {
       try {
         const recordData = await getLearnRecords();
         console.log(recordData)
+        console.log(recordData)
         setRecords(recordData);
+        setTotalTime(totalTimeCalculate(recordData));
       } catch (error) {
         console.error("データの取得に失敗しました", error);
       } finally {
@@ -27,7 +33,7 @@ export const App = () => {
     data();
   }, []);
 
-  const handleChange = (e) => {
+  const handleInputChange = (e) => {
     const { name, value } = e.target;
     if (name === "time" && !/^[0-9]*$/.test(value)) {
       return;
@@ -40,41 +46,31 @@ export const App = () => {
     }));
   };
 
-  const handleRegister = async () => {
-    // let newRecord;
+  const handleRecord = async () => {
     //学習内容もしくは学習時間のどちらかが入力されていないときに入力されていない項目がありますと表示する
     if (!inputs.content || inputs.time === "" || inputs.time === null) {
       setError("入力されていない項目があります。");
       return;
     } else {
-      //入力した学習内容、学習時間をオブジェクトの中に入れる
-      //学習時間は数値型で10進数に変換
-      // newRecord = {
-      //   content: inputs.content,
-      //   time: parseInt(inputs.time, 10),
-      // };
 
       try {
         //supabaseのテーブルのcontent,timeというカラムに入力した学習内容、学習時間を登録する
+        //学習時間は数値型で10進数に変換
         const { data, error } = await supabase
           .from("study-record")
           .insert({ content: inputs.content, time: parseInt(inputs.time, 10) })
-          .select().single();
+          .select().single()
         //エラーがあれば例外を投げる
         if (error) {
           throw error;
         }
-        console.log(data);
+        // console.log(data);
           //入力したデータをリストに追加する
     const newRecords = [...records, data];
     setRecords(newRecords);
     console.log(newRecords);
-    //合計時間を計算
-    const newTotalTime = newRecords.reduce(
-      (sum, record) => sum + record.time,
-      0
-    );
-    setTotalTime(newTotalTime);
+    //合計時間を再計算
+    setTotalTime(totalTimeCalculate(newRecords));
       } catch (error) {
         //正常にデータが登録できなかった際にエラーをコンソールに表示する
         console.error("Error insert addData:", error);
@@ -103,6 +99,8 @@ export const App = () => {
       console.log('newRecords length:', newRecords[0]);
       console.log("filteredRecords:", newRecords);
       setRecords([...newRecords]);
+      //合計時間を再計算して表示
+      setTotalTime(totalTimeCalculate(newRecords));
       console.log("削除後のrecords:", setRecords);
       console.log("IDの型:", typeof id); 
       console.log("IDの内容:", id);
@@ -122,17 +120,17 @@ export const App = () => {
     <h2>Loading...</h2>
   ) : (
     <>
-      <h1>学習記録一覧</h1>
+      <h1>学習記録リスト</h1>
       <InputForm
         name="content"
         value={inputs.content}
-        onChange={handleChange}
+        onChange={handleInputChange}
         label="学習内容"
       />
       <InputForm
         name="time"
         value={inputs.time}
-        onChange={handleChange}
+        onChange={handleInputChange}
         label="学習時間"
         suffix="時間"
         pattern="^[0-9]*$"
@@ -140,10 +138,9 @@ export const App = () => {
       <p>入力されている学習内容: {inputs.content}</p>
       <p>入力されている時間: {inputs.time}時間</p>
       <RecordData data={records} onClickDelete={deleteRecord}/>
-      <button onClick={handleRegister}>登録</button>
+      <button onClick={handleRecord}>登録</button>
       <div>{error}</div>
       <p>合計時間：{totalTime}/1000（h）</p>
-      {/* <ListDisplay recordList={records} /> */}
     </>
   );
 };
