7b9bd1be75821896e90bb7293797c4d3881d434a
miduki-gif <midzuki1935@gmail.com>
2024-09-11 22:19:37 +0900

    deploy-make

--- utils/Supabase.js
+++ utils/Supabase.js
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 // Create a single supabase client for interacting with your database
 //初期化
 
-const supabaseUrl = import.meta.env.VITE_NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(
     supabaseUrl,
