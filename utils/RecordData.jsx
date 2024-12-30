2a8b73919ea01206b3d02f88fec550e2c7b4be88
miduki-gif <midzuki1935@gmail.com>
2024-12-31 06:54:32 +0900

    Delete blank line

--- utils/RecordData.jsx
+++ utils/RecordData.jsx
@@ -4,9 +4,8 @@ export const RecordData = (props) =>{
         <>
         <ul style={{listStyle:"none"}}>
         {data.map((record) => (
-          <li key={record.id}>{record.content}{record.time}時間
+          <li data-testid="recordData" key={record.id}>{record.content}{record.time}時間
           <button onClick={() => onClickDelete(record.id)}>削除</button>
-
           </li>
         )
         )}
